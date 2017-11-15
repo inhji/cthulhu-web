df7205ad379bcd42eeb4f6606612237812b89f70
Jonathan Jenne <johnnie@posteo.de>
2017-11-15 18:08:43 +0100

    clean up

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { render, hydrate } from "react-dom"
+import { render } from "react-dom"
 import { BrowserRouter } from "react-router-dom"
 import { ApolloProvider } from "react-apollo"
 import { MuiThemeProvider, createMuiTheme } from "material-ui/styles"
@@ -11,28 +11,15 @@ import registerServiceWorker from "./registerServiceWorker"
 const theme = createMuiTheme()
 const root = document.getElementById("root")
 
-if (root.hasChildNodes()) {
-  hydrate(
-    <BrowserRouter>
-      <ApolloProvider client={client}>
-        <MuiThemeProvider theme={theme}>
-          <App />
-        </MuiThemeProvider>
-      </ApolloProvider>
-    </BrowserRouter>,
-    root
-  )
-} else {
-  render(
-    <BrowserRouter>
-      <ApolloProvider client={client}>
-        <MuiThemeProvider theme={theme}>
-          <App />
-        </MuiThemeProvider>
-      </ApolloProvider>
-    </BrowserRouter>,
-    document.getElementById("root")
-  )
-}
+render(
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <MuiThemeProvider theme={theme}>
+        <App />
+      </MuiThemeProvider>
+    </ApolloProvider>
+  </BrowserRouter>,
+  document.getElementById("root")
+)
 
 registerServiceWorker()
