d117a81ca10a7cb19e17de07e39421166b1c3c45
Jonathan Jenne <johnnie@posteo.de>
2017-11-15 23:19:19 +0100

    put graphcool endpoint in env file

--- src/apollo.js
+++ src/apollo.js
@@ -4,8 +4,7 @@ import { setContext } from "apollo-link-context"
 import { InMemoryCache } from "apollo-cache-inmemory"
 import { GC_AUTH_TOKEN } from "./constants"
 
-const projectId = process.env.NODE_ENV === "production" ? "cthulhu-prod" : "cthulhu-devel"
-const httpLink = new HttpLink({ uri: `https://api.graph.cool/simple/v1/${projectId}` })
+const httpLink = new HttpLink({ uri: process.env.REACT_APP_GRAPHCOOL_ENDPOINT })
 
 const middlewareLink = setContext(() => ({
   headers: {
