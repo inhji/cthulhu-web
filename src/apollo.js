ab5245f8a482e27746150e9bd9523d1d6fd12b51
Inhji <johnnie@posteo.de>
2017-12-10 15:04:31 +0100

    fix sending of auth header

--- src/apollo.js
+++ src/apollo.js
@@ -1,19 +1,18 @@
 import { ApolloClient } from 'apollo-client'
+import { ApolloLink } from 'apollo-link'
 import { HttpLink } from 'apollo-link-http'
-import { setContext } from 'apollo-link-context'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { GC_AUTH_TOKEN } from './constants'
 
 const httpLink = new HttpLink({ uri: process.env.REACT_APP_GRAPHCOOL_ENDPOINT })
-const authLink = setContext((req, ctx) => {
-  const headers = ctx.headers || {}
-  const token = localStorage.getItem(GC_AUTH_TOKEN)
 
-  if (token) {
-    headers.Authorization = `Bearer ${token}`
-  }
-
-  return headers
+const authLink = new ApolloLink((operation, forward) => {
+  operation.setContext(({ headers }) => ({
+    headers: localStorage.getItem(GC_AUTH_TOKEN)
+      ? { authorization: `Bearer ${localStorage.getItem(GC_AUTH_TOKEN)}` }
+      : {}
+  }))
+  return forward(operation)
 })
 
 const link = authLink.concat(httpLink)
